3c655fd3b7d3dc29d49d1ae2ae3062dc37346bee
Keast27 <keast1027@gmail.com>
2020-10-29 11:41:45 -0400

    Added explosion effect?

---
+++ Hello World VR/Assets/Scripts/Boom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+{
+    public GameObject particles;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Floor")
+        {
+            Vector3 spawn = transform.position;
+            spawn.y = 1;
+            Instantiate(particles, spawn, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
