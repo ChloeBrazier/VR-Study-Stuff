dff1637cc73b512b7d7f180f5811f841c091112d
Keast27 <keast1027@gmail.com>
2020-10-29 01:07:07 -0400

    Added y adjustmet to peel

--- Hello World VR/Assets/Scripts/Banana.cs
+++ Hello World VR/Assets/Scripts/Banana.cs
@@ -6,14 +6,17 @@ public class Banana : MonoBehaviour
 {
 
     public GameObject peel;
-
+    //y 0.04831seven9
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Floor")
         {
-            Instantiate(peel, transform.position, Quaternion.identity);
+            Vector3 spawn = transform.position;
+            spawn.y = 0;
+            Instantiate(peel, spawn, Quaternion.identity);
             Destroy(gameObject);
         }
     }
-
+    //y0 = 
+    //yf =
 }
