fc688f747662091510613d70cda0ba882b32e5ee
Keast27 <keast1027@gmail.com>
2020-10-29 00:56:04 -0400

    Banana should peel

---
+++ Hello World VR/Assets/Scripts/Banana.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banana : MonoBehaviour
+{
+
+    public GameObject peel;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Floor")
+        {
+            Instantiate(peel, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+}
