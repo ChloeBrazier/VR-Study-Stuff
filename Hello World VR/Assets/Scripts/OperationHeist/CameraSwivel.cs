87abf976c34451b976f362be24ac22e14e27460f
Eddie Brazier <edwinbrazier01@gmail.com>
2020-11-15 21:12:10 -0500

    set up basic hacker screen for heist game

---
+++ Hello World VR/Assets/Scripts/OperationHeist/CameraSwivel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwivel : MonoBehaviour
+{
+    //start and end rotations
+    public Transform startRotation;
+    public Transform endRotation;
+    private bool canRotate = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canRotate)
+        {
+            StartCoroutine(Swivel());
+            canRotate = false;
+        }
+    }
+
+    public IEnumerator Swivel()
+    {
+        for (float i = 0; i < 1.0f; i += Time.deltaTime/4)
+        {
+            //lerp camera rotation
+            transform.rotation = Quaternion.Lerp(startRotation.rotation, endRotation.rotation, i);
+            yield return null;
+        }
+
+        //switch start and end rotation
+        Transform tempRotation = startRotation;
+        startRotation = endRotation;
+        endRotation = tempRotation;
+
+        //stop camera from rotating for a second
+        yield return new WaitForSeconds(1.0f);
+
+        //set rotation bool
+        canRotate = true;
+    }
+}
---
+++ Hello World VR/Assets/Scripts/OperationHeist/HackerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HackerController : MonoBehaviour
+{
+    //public list of render textures
+    public List<RenderTexture> securityCamList;
+
+    //security cam feed for hacker
+    public RawImage activeCam;
+    private int camIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set defualt active security cam
+        activeCam.texture = securityCamList[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: Make this work with UI buttons
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SwitchCam();
+        }
+    }
+
+    public void SwitchCam()
+    {
+        camIndex++;
+        if(camIndex == securityCamList.Count)
+        {
+            camIndex = 0;
+        }
+
+        activeCam.texture = securityCamList[camIndex];
+    }
+
+    public void SetCam(int index)
+    {
+        activeCam.texture = securityCamList[index];
+    }
+}
