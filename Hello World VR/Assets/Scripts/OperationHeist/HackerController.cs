cfdf021faf393f88f20392056bd32d904258ca7e
Eddie Brazier <edwinbrazier01@gmail.com>
2020-11-16 22:08:38 -0500

    fleshed out test rooms and hacker UI

--- Hello World VR/Assets/Scripts/OperationHeist/HackerController.cs
+++ Hello World VR/Assets/Scripts/OperationHeist/HackerController.cs
@@ -8,6 +8,11 @@ public class HackerController : MonoBehaviour
     //public list of render textures
     public List<RenderTexture> securityCamList;
 
+    //lists for room-specific buttons
+    public List<GameObject> roomOneButtons;
+    public List<GameObject> roomTwoButtons;
+    public List<GameObject> roomThreeButtons;
+
     //security cam feed for hacker
     public RawImage activeCam;
     private int camIndex = 0;
@@ -43,5 +48,69 @@ public class HackerController : MonoBehaviour
     public void SetCam(int index)
     {
         activeCam.texture = securityCamList[index];
+    }
+
+    public void OpenDoor(GameObject door)
+    {
+        Destroy(door);
+    }
+
+    //ugly-ass method for switching rooms p l e as e forgive me
+    public void SwitchRoom(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                //set room one buttons active and room two and three buttons inactive
+                foreach(GameObject button in roomTwoButtons)
+                {
+                    button.SetActive(false);
+                }
+
+                foreach (GameObject button in roomThreeButtons)
+                {
+                    button.SetActive(false);
+                }
+
+                foreach (GameObject button in roomOneButtons)
+                {
+                    button.SetActive(true);
+                }
+                break;
+            case 1:
+                //set room two buttons active and room one and three buttons inactive
+                foreach (GameObject button in roomOneButtons)
+                {
+                    button.SetActive(false);
+                }
+
+                foreach (GameObject button in roomThreeButtons)
+                {
+                    button.SetActive(false);
+                }
+
+                foreach (GameObject button in roomTwoButtons)
+                {
+                    button.SetActive(true);
+                }
+                break;
+            case 2:
+                //set room three buttons active and room one and two buttons inactive
+                foreach (GameObject button in roomOneButtons)
+                {
+                    button.SetActive(false);
+                }
+
+                foreach (GameObject button in roomTwoButtons)
+                {
+                    button.SetActive(false);
+                }
+
+                foreach (GameObject button in roomThreeButtons)
+                {
+                    button.SetActive(true);
+                }
+                break;
+        }
     }
 }
